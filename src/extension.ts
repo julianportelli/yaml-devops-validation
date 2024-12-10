9f368567bda54dc348861d90d21f1c9cb2faa043
Julian Portelli <portelli.jul@gmail.com>
2024-12-11 00:19:35 +0100

    Working debuggable version

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('yaml-devops-validation.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from YAML DevOps Validation!');
+		vscode.window.showInformationMessage('Hello VS Code!');
 	});
 
 	context.subscriptions.push(disposable);

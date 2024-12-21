b5a6ba9f2132739c13e539ddb7fe5aa086c47cc6
Julian Portelli <julian.a.portelli@contractors.gov.mt>
2024-12-21 16:45:40 +0100

    Restructuring

--- src/extension.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-import AzurePipelinesValidator from './BusinessLogic/AzurePipelinesValidatior';
-
-export function activate(context: vscode.ExtensionContext) {
-	const validator = new AzurePipelinesValidator(context);
-	validator.activate(context);
-}
-
-export function deactivate() { }
